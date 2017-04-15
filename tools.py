7b5dca6a4c7ea4990a80be3acb33416a8256307b
徐遥 <kevin6241@gmail.com>
2017-04-14 22:52:12 -0400

    change to OOP style

--- tools.py
+++ tools.py
@@ -3,21 +3,61 @@
 Helper functions
 '''
 import re
+import jieba
+from collections import Counter
 
 
-def chapter_list(text):
-    '''
-    seperate text in to chapters with regular expressions
-    return splitted chapters in list
-    '''
-    sep = re.compile(r'第.*?回\s')
-    return sep.split(text)[1:]
+def isHan(text):
+    'If text is contains Chinese characters only'
+    return all('\u4e00' <= char <= '\u9fff' for char in text)
+
+
+class Chapter():
+    'Class used to store and process a chapter'
+    text = ''
+    words = dict()
+    num_words = 0
+
+    def __init__(self, text):
+        self.text = text
+        self.words = self.__get_words(self.text)
+        self.num_words = len(self.words)
+
+    def __get_words(self, text):
+        'split text into words and store them in a dictionary'
+        word_bracket = jieba.cut(text)
+        filtered = list(filter(isHan, word_bracket))
+        return dict(Counter(filtered))
+
+
+class Novel():
+    'Class used to store and process a novel'
+    text = ''
+    chapters = list()
+    num_chapters = 0
+
+    def __init__(self, filename):
+        f = open(filename, 'r', encoding='utf-8')
+        self.text = f.read()
+        self.chapters = self.__chapter_list(self.text)
+        self.num_chapters = len(self.chapters)
+
+    def __chapter_list(self, text):
+        '''
+        seperate text in to chapters with regular expressions
+        return splitted chapters in list
+        '''
+        sep = re.compile(r'第.*?回\s')
+        raw_chapters = sep.split(text)[1:]
+        return list(map(Chapter, raw_chapters))
 
 
 if __name__ == '__main__':
-    f = open('hongloumeng.txt', 'r', encoding='utf-8')
-    text = f.read()
-    chapters = chapter_list(text)
-    print(len(chapters))
-    for i in range(len(chapters)):
-        print('chapter ' + str(i + 1) + ': ', len(chapters[i]), 'words')
+    n = Novel('hongloumeng.txt')
+    print('Number of chapters: ', n.num_chapters)
+    c = n.chapters[0]
+    print('Content of first chapter')
+    print(c.text)
+    print('Number of unique words in first chapter: ', c.num_words)
+    print('Words list')
+    print(c.words)
